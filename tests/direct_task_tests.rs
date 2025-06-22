abfe6b8ab6be5f0be5744070c2e3d94181e79e7c
Jaeyong Sung <jaeyong0201@gmail.com>
2025-06-22 23:03:13 +0900

    add test for direct test macro

---
+++ tests/direct_task_tests.rs
@@ -0,0 +1,67 @@
+use std::thread;
+
+// New test for direct task implementation
+use namu::prelude::*;
+use namu_core::{Task, TaskContext};
+use namu_engine::context::dynamic_context::DynamicContextManager;
+use namu_engine::engine::Engine;
+use namu_engine::engine::simple_engine::SimpleEngine;
+use namu_macros::task_bridge;
+
+#[task]
+#[derive(Default, Clone)]
+struct DoubleTask;
+
+impl<C: TaskContext> Task<C> for DoubleTask {
+    fn prepare(&mut self) -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn clone_boxed(&self) -> Box<dyn Task<C> + Send + Sync> {
+        Box::new(self.clone())
+    }
+
+    fn run(&mut self, ctx: C) -> anyhow::Result<()> {
+        while let Ok((id, x)) = ctx.recv::<i32>() {
+            let _ = ctx.send(id, Ok(x * 2));
+            let _ = ctx.send_end(id);
+        }
+        Ok(())
+    }
+}
+
+task_bridge! {
+    fn_name = double_direct,
+    task    = DoubleTask,
+    inputs  = "i32",
+    output  = "i32",
+    name    = "double_direct",
+    author  = "namu",
+    version = "0.1",
+}
+
+#[workflow]
+fn wf() -> i32 {
+    let x = 4;
+    double_direct(x)
+}
+
+#[test]
+fn direct_task_workflow_builds() {
+    let graph = wf().to_serializable("workflow".to_string());
+    let engine = SimpleEngine::with_registered(DynamicContextManager::new());
+    let workflow_id = engine.create_workflow(graph);
+    let run_id = engine.create_run(workflow_id);
+    let engine_clone = engine.clone();
+    let handle = thread::spawn(move || {
+        engine_clone.run(run_id);
+    });
+    let result = *engine
+        .get_result(run_id)
+        .next()
+        .unwrap()
+        .downcast_ref::<i32>()
+        .unwrap();
+    handle.join().unwrap();
+    assert_eq!(result, 8);
+}
